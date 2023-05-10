54191204268501d2494e8f7d373481e4e4a89688
Omkar <109603669+sagavekar@users.noreply.github.com>
2023-05-11 05:21:32 +0530

    Improvements added
    1. better GUI
    2. case sensitive check box
    3. removed matching file label
    4. added error handling
    5. user can directly open file by doubl clicking

--- Text file search.py
+++ Text file search.py
@@ -1,13 +1,20 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 import re
 import os
-
+import subprocess
 
 def select_files():
+    
     initial_dir = os.path.expanduser("~")  # Set initial directory to user's home directory
     files = filedialog.askopenfilenames(initialdir=initial_dir, filetypes=[("Text Files", "*.txt")])
     file_list.delete(0, tk.END)  # Clear the existing list
+    
+    file_count = len(files)
+    file_count_label.config(text=f"{file_count}")
+    
+    
     for file_path in files:
         file_name = os.path.basename(file_path)
         file_list.insert(tk.END, file_name)
@@ -15,41 +22,59 @@ def select_files():
     if files:
         global current_dir
         current_dir = os.path.dirname(files[0])
-
+        
 def search_files():
     keyword = keyword_entry.get()
     file_matches.delete(0, tk.END)  # Clear the existing list
-
+    
     match_found = False  # Variable to track if any match is found
+    num_files = file_list.size()
 
-    for i in range(file_list.size()):
+    for i in range(num_files):
         file_name = file_list.get(i)
         file_path = os.path.join(current_dir, file_name)
-        with open(file_path, 'r') as file:
-            content = file.read()
-            matches = re.findall(keyword, content)
-            if matches:
-                file_matches.insert(tk.END, file_name)
-                match_found = True
+        try:
+            with open(file_path, 'r') as file:
+                content = file.read()
+                matches = re.findall(keyword, content)
+                if matches:
+                    file_matches.insert(tk.END, file_name)
+                    match_found = True
+        except (IOError, OSError) as e:
+            file_matches.insert(tk.END, f"Error reading file: {file_name}")
+            print(f"Error reading file: {file_name}")
+            print(e)
+        
+    file_matches.bind("<Double-Button-1>", open_selected_file)
 
+    
     if not match_found:
-        file_matches.insert(tk.END, "No match found, Plese note that Keyword in case sensative")
+        file_matches.insert(tk.END, "No match found. Please note that the keyword is case-sensitive.")
+        
+
+
+
+def open_selected_file(event):
+    selected_file = file_matches.get(file_matches.curselection())
+    file_path = os.path.join(current_dir, selected_file)
+    os.startfile(file_path)
 
 def clear_all():
     file_list.delete(0, tk.END)
     keyword_entry.delete(0, tk.END)
     file_matches.delete(0, tk.END)
+    
 
 window = tk.Tk()
-window.title("Text File Search | Omka Sagavekar | TSO-SET ")
+window.title("Text File Search | Omkar.Sagavekar@gep.com | TSO-SET ")
 
 # Styling
-window.geometry("500x300")
+window.geometry("540x300")
 window.configure(background="#F0F0F0")
 
 # File Selection
 file_frame = tk.Frame(window, bg="#F0F0F0")
-file_frame.pack(pady=5)
+file_frame.pack(pady=3)
 
 file_list = tk.Listbox(file_frame, selectmode=tk.MULTIPLE, font=("Arial", 11), width=600, height=6)
 file_list.pack(fill=tk.BOTH, expand=True)
@@ -57,23 +82,33 @@ file_list.pack(fill=tk.BOTH, expand=True)
 
 # Button Frame
 button_frame = tk.Frame(file_frame, bg="#F0F0F0")
-button_frame.pack(pady=5)
+button_frame.pack(pady=3)
 
-select_button = tk.Button(button_frame, text="Select Files", command=select_files, font=("Arial", 12))
-select_button.pack(side=tk.LEFT, padx=20, pady=5)
+select_button = tk.Button(button_frame, text="Select Files", command=select_files, font=("Arial", 11))
+select_button.pack(side=tk.LEFT, padx=9, pady=3)
 
-search_button = tk.Button(button_frame, text="Search", command=search_files, font=("Arial", 12))
-search_button.pack(side=tk.LEFT, padx=20, pady=5)
+search_button = tk.Button(button_frame, text="Search", command=search_files, font=("Arial", 11))
+search_button.pack(side=tk.LEFT, padx=9, pady=3)
 
-clear_button = tk.Button(button_frame, text="Reset", command=clear_all, font=("Arial", 12))
-clear_button.pack(side=tk.LEFT, padx=20, pady=5)
+Reset_button = tk.Button(button_frame, text="Reset", command=clear_all, font=("Arial", 11))
+Reset_button.pack(side=tk.LEFT, padx=9, pady=3)
+
+case_sensitive_var = tk.IntVar()
+case_sensitive_checkbox = tk.Checkbutton(button_frame, text="Case Insensitive", variable=case_sensitive_var, bg="#F0F0F0", font=("Arial", 11))
+case_sensitive_checkbox.pack(side=tk.LEFT)
+
+
+file_count_label = tk.Label(button_frame, text=" ", bg="#F0F0F0", font=("Arial", 11))
+file_count_label.pack(side=tk.LEFT, padx=9, pady=3)
+
+
 
 
 # Keyword Search
 keyword_frame = tk.Frame(window, bg="#F0F0F0")
 keyword_frame.pack(pady=5)
 
-keyword_label = tk.Label(keyword_frame, text="Keyword:", bg="#F0F0F0", font=("Arial", 12))
+keyword_label = tk.Label(keyword_frame, text="Keyword:", bg="#F0F0F0", font=("Arial", 11))
 keyword_label.pack(side=tk.LEFT)
 
 keyword_entry = tk.Entry(keyword_frame, font=("Arial", 11), width=30)
@@ -82,8 +117,8 @@ keyword_entry.pack(side=tk.LEFT)
 
 
 # Result Display
-result_label = tk.Label(window, text="Matching Files:", bg="#F0F0F0", font=("Arial", 12))
-result_label.pack()
+# result_label = tk.Label(window, text="Matching Files:", bg="#F0F0F0", font=("Arial", 11))
+# result_label.pack()
 
 file_matches = tk.Listbox(window, font=("Arial", 11), width=50, height=8)
 file_matches.pack(fill=tk.BOTH, expand=True)

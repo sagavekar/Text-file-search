4e99e33ae93368fd8fca425d74cfaea5b0941552
Omkar <109603669+sagavekar@users.noreply.github.com>
2023-05-11 04:30:05 +0530

    Dynamic directory with button alignment

--- Text file search.py
+++ Text file search.py
@@ -3,7 +3,6 @@ from tkinter import filedialog
 import re
 import os
 
-flag_found = 0
 
 def select_files():
     initial_dir = os.path.expanduser("~")  # Set initial directory to user's home directory
@@ -13,9 +12,15 @@ def select_files():
         file_name = os.path.basename(file_path)
         file_list.insert(tk.END, file_name)
 
+    if files:
+        global current_dir
+        current_dir = os.path.dirname(files[0])
+
 def search_files():
     keyword = keyword_entry.get()
     file_matches.delete(0, tk.END)  # Clear the existing list
+
+    match_found = False  # Variable to track if any match is found
 
     for i in range(file_list.size()):
         file_name = file_list.get(i)
@@ -24,25 +29,45 @@ def search_files():
             content = file.read()
             matches = re.findall(keyword, content)
             if matches:
-                flag_found = 1
                 file_matches.insert(tk.END, file_name)
+                match_found = True
+
+    if not match_found:
+        file_matches.insert(tk.END, "No match found, Plese note that Keyword in case sensative")
+
+def clear_all():
+    file_list.delete(0, tk.END)
+    keyword_entry.delete(0, tk.END)
+    file_matches.delete(0, tk.END)
 
 window = tk.Tk()
-window.title("Text File Search")
+window.title("Text File Search | Omka Sagavekar | TSO-SET ")
 
 # Styling
-window.geometry("500x400")
+window.geometry("500x300")
 window.configure(background="#F0F0F0")
 
 # File Selection
 file_frame = tk.Frame(window, bg="#F0F0F0")
 file_frame.pack(pady=5)
 
-file_list = tk.Listbox(file_frame, selectmode=tk.MULTIPLE, font=("Arial", 11), width=60, height=6)
+file_list = tk.Listbox(file_frame, selectmode=tk.MULTIPLE, font=("Arial", 11), width=600, height=6)
 file_list.pack(fill=tk.BOTH, expand=True)
 
-file_button = tk.Button(file_frame, text="Select Files", command=select_files, font=("Arial", 12))
-file_button.pack (side="bottom",pady=3)
+
+# Button Frame
+button_frame = tk.Frame(file_frame, bg="#F0F0F0")
+button_frame.pack(pady=5)
+
+select_button = tk.Button(button_frame, text="Select Files", command=select_files, font=("Arial", 12))
+select_button.pack(side=tk.LEFT, padx=20, pady=5)
+
+search_button = tk.Button(button_frame, text="Search", command=search_files, font=("Arial", 12))
+search_button.pack(side=tk.LEFT, padx=20, pady=5)
+
+clear_button = tk.Button(button_frame, text="Reset", command=clear_all, font=("Arial", 12))
+clear_button.pack(side=tk.LEFT, padx=20, pady=5)
+
 
 # Keyword Search
 keyword_frame = tk.Frame(window, bg="#F0F0F0")
@@ -54,8 +79,7 @@ keyword_label.pack(side=tk.LEFT)
 keyword_entry = tk.Entry(keyword_frame, font=("Arial", 11), width=30)
 keyword_entry.pack(side=tk.LEFT)
 
-search_button = tk.Button(window, text="Search", command=search_files, font=("Arial", 12))
-search_button.pack(pady=5)
+
 
 # Result Display
 result_label = tk.Label(window, text="Matching Files:", bg="#F0F0F0", font=("Arial", 12))
@@ -64,7 +88,6 @@ result_label.pack()
 file_matches = tk.Listbox(window, font=("Arial", 11), width=50, height=8)
 file_matches.pack(fill=tk.BOTH, expand=True)
 
-# Get the current directory
-current_dir = os.getcwd()
+
 
 window.mainloop()
